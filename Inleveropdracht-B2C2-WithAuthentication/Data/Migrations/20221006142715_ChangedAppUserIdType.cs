3ac66bb2ce5d34558e3cd419f5e7ec5a6de7a54c
Justin Dang <2106209dang@zuyd.nl>
2022-10-06 16:44:40 +0200

    Added AppUser

---
+++ Inleveropdracht-B2C2-WithAuthentication/Data/Migrations/20221006142715_ChangedAppUserIdType.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inleveropdracht_B2C2_WithAuthentication.Data.Migrations
+{
+    public partial class ChangedAppUserIdType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
